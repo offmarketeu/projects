d82eec756ff16450bccaa7841683571b10ea3746
offmarketeu <analisis@offmarket.eu>
2018-08-28 23:40:47 +0200

    Add files via upload

--- pincho.r
+++ pincho.r
@@ -15,7 +15,12 @@ nnd<- url %>%
 # uso de xml
 url2<-"http://www.cnmv.es/Portal/Consultas/IIC/Fondo.aspx?nif=V85405082&vista=1&fs=28/08/2018"
 html<- htmlTreeParse(url2, useInternalNodes=T)
-xpathSApply(html, "td data")
+id<-xpathSApply(html, "//td/a", xmlGetAttr, 'id')
+idd<- data.frame(unlist(id))
+href<-xpathSApply(html, "//td/a", xmlGetAttr, 'href')
+hhref<- data.frame(unlist(href))
+
+
 txt<-"<a id="ctl00_ContentPrincipal_wIPPS_gridIPPS_ctl02_lnkPDF" href="../../verDoc.axd?t={24192b85-fb9e-4655-9905-57153dc4be41}" target="_blank">"
 txt2<-"http://www.cnmv.es/Portal/verDoc.axd?t={e8373b10-8550-4fda-9141-c42a69362024}"
 
