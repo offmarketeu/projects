90685d9020b8c28a0573a4283fbb2c439b5948d5
offmarketeu <analisis@offmarket.eu>
2018-08-28 08:46:49 +0200

    Add files via upload

--- pincho.r
+++ pincho.r
@@ -1,16 +1,25 @@
 library(tabulizer)
 library(dplyr)
 library(XML)
+library(rvest)
 
 nif <- read.csv2("E:/MODMAT/Proyect_pincho/PROV_MUTFUND2.csv")
 
-url<-"http://www.cnmv.es/Portal/Consultas/IIC/Fondo.aspx?nif=V85405082&vista=1&fs=26/08/2018"
-html<- htmlTreeParse(url, useInternalNodes=T)
-xpathSApply(html, "",
+# uso de rvest
+url<-read_html("http://www.cnmv.es/Portal/Consultas/IIC/Fondo.aspx?nif=V85405082&vista=1&fs=28/08/2018")
+nnd<- url %>%
+  	html_nodes(".td data") %>%
+  	html_text() %>%
+  	as.character()
+
+# uso de xml
+url2<-"http://www.cnmv.es/Portal/Consultas/IIC/Fondo.aspx?nif=V85405082&vista=1&fs=28/08/2018"
+html<- htmlTreeParse(url2, useInternalNodes=T)
+xpathSApply(html, "td data")
 txt<-"<a id="ctl00_ContentPrincipal_wIPPS_gridIPPS_ctl02_lnkPDF" href="../../verDoc.axd?t={24192b85-fb9e-4655-9905-57153dc4be41}" target="_blank">"
 txt2<-"http://www.cnmv.es/Portal/verDoc.axd?t={e8373b10-8550-4fda-9141-c42a69362024}"
 
-
+# extrae la tabla de valores
 doc<- "E:/MODMAT/Proyect_pincho/verDoc2.pdf"
 dtable<- extract_tables(doc)
 tex <- length(dtable)
