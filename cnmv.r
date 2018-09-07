ea08396427fce3a5fc0489067cd213c09d438b74
offmarketeu <analisis@offmarket.eu>
2018-09-07 09:07:32 +0200

    Add files via upload

--- cnmv.r
+++ cnmv.r
@@ -3,11 +3,20 @@ library(tidyr)
 
 file1<- "E:/01_ADMON_OFFMARKET/CNMV/FONDCART.csv"
 file2<- "E:/01_ADMON_OFFMARKET/CNMV/FONDREGISTRO.csv"
-file3<- "E:/01_ADMON_OFFMARKET/CNMV/FONDMES.csv"
+file3<- "E:/01_ADMON_OFFMARKET/CNMV/FONDMES092017.csv"
+dias<- "E:/01_ADMON_OFFMARKET/CNMV/dias.csv"
+
 
 f1 <- read.csv2(file1,header=TRUE)
 f2 <- read.csv2(file2,header=TRUE)
 f3 <- read.csv2(file3,header=TRUE)
+df <- read.csv2(dias,header=TRUE)
+
+# convertidor de fecha - CAMBIAR CADA VEZ
+ddias <- as.Date("2017-09-01") + (0:30)
+df<- data.frame(df, ddias)
+colnames(df)<- c("FEC", "FECHA")
+
 
 # Sustituir numero de registro por isin
 f1$NumeroRegistro <- f2$ISIN[match(f1$NumeroRegistro, f2$NumeroRegistro)]
@@ -22,3 +31,7 @@ f1bis<- f1 %>% separate(DescripcionValor, c("Clase","Emisor","Cupon","Venci"), f
 f3<- f3[,c(1:37)]
 f3<- f3[,-c(1:5)]
 f3bis<- gather(f3, ISIN)
+colnames(f3bis)<- c("ISIN", "FEC", "value")
+f3bis$FEC <- df$FECHA[match(f3bis$FEC, df$FEC)]
+f3bis<- f3bis[, c(1,3,2,2)]
+write.csv2(f3bis, "E:/01_ADMON_OFFMARKET/CNMV/vl_092017.csv", row.names=FALSE)
